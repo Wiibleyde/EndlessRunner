acb39c8afc8f0efdb9e3fe65608abfedc2e19ff1
Wiibleyde <nathan@bonnell.fr>
2024-01-16 10:39:45 +0100

    Update Spawner position and scale
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    private float leftEdge;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 2f;
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (transform.position.x < leftEdge) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Spawnable objects
+    public GameObject[] spawnableObjects;
+    // Spawn interval
+    public float[] spawnRangeInterval = { 1f, 2f };
+
+    void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    IEnumerator SpawnObjects()
+    {
+        while (true)
+        {
+            Instantiate(spawnableObjects[Random.Range(0, spawnableObjects.Length)], transform.position, Quaternion.identity);
+            float waitTime = Random.Range(spawnRangeInterval[0], spawnRangeInterval[1]);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
