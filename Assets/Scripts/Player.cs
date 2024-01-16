b2777d879ca68f3a063c5cce3d3dfd81293b81bc
Wiibleyde <nathan@bonnell.fr>
2024-01-16 15:05:08 +0100

    Update player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,12 +23,10 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Obstacle")) {
             Debug.Log("Game Over");
         }
     }
-
 }
- 
